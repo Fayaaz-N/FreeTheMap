4b15264eb1beacae7dd063cf07c698d2516910c6
fayaaz-n <fayaaz1605@gmail.com>
2025-12-17 23:19:52 +0100

    Python fetch voor alle spelers
    nog te doen:
    Logo's invullen
    Data opschonen

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,23 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './js/app.js',
+    app: "./js/app.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    filename: './js/app.js',
+    filename: "js/app.js",
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "index.html", to: "index.html" },
+        { from: "css", to: "css" },
+        { from: "img", to: "img" },
+        { from: "js/data.json", to: "js/data.json" },
+      ],
+    }),
+  ],
 };
