cef4d0a5b64e1147252349b960bafc181d2b6a1d
fayaaz-n <fayaaz1605@gmail.com>
2025-12-19 00:35:55 +0100

    Python fetch voor alle spelers
    nog te doen:
    Logo's invullen
    Data opschonen
    
    NUCOMMIT

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-module.exports = {
-  entry: { app: "./js/app.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    filename: "js/app.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-      inject: "body",
-    }),
-  ],
-};
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+//
+// module.exports = {
+//   entry: { app: "./js/app.js" },
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//     clean: true,
+//     filename: "js/app.js",
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./index.html",
+//       inject: "body",
+//     }),
+//   ],
+// };
